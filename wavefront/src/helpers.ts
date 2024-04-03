80a7ff0aeec8022ef6efc445edde18b33cbe08a2
Heesho <100749457+Heesho@users.noreply.github.com>
2024-04-03 13:29:51 -0700

    updated multicall address for graph

--- wavefront/src/helpers.ts
+++ wavefront/src/helpers.ts
@@ -2,7 +2,7 @@ import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 export const FACTORY_ADDRESS = "0xE0b5239cb1e29cA341Ae9D0cD576268ebE0047aE";
-export const MULTICALL_ADDRESS = "0x35Fa5848bE77B07578Cc232370a74A5c4cbdbc47";
+export const MULTICALL_ADDRESS = "0x7587D5Dd60Da6da42E86CC7d78cB80E12Fe61167";
 
 export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
