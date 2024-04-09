640398b59244dbf7bdef46be45a91115ed5ccfaf
Heesho <hisham11027@gmail.com>
2024-04-08 17:11:54 -0700

    update status fee to 1000

--- contracts/MemeFactory.sol
+++ contracts/MemeFactory.sol
@@ -178,7 +178,7 @@ contract Meme is ERC20, ERC20Permit, ERC20Votes, ReentrancyGuard {
     uint256 public constant INITIAL_SUPPLY = 1000000000 * PRECISION; // Initial supply of the meme token
     uint256 public constant FEE = 200; // 2% fee rate for buy/sell operations
     uint256 public constant FEE_AMOUNT = 2000; // Additional fee parameters for ditributing to stakeholders
-    uint256 public constant STATUS_FEE = 10 * PRECISION; // Fee for status update 10 tokens
+    uint256 public constant STATUS_FEE = 1000 * PRECISION; // Fee for status update 10 tokens
     uint256 public constant STATUS_MAX_LENGTH = 280; // Maximum length of status string
     uint256 public constant DIVISOR = 10000; // Divisor for fee calculations
 
